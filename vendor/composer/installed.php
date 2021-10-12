40c738502c26cc56ea893271f08079ae3f7129cd
Kyohsuke-Ono926 <kyoh4k-gongyou@kuc.biglobe.ne.jp>
2021-10-12 22:29:23 +0900

    composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '357d454c71fdb57c312ccc2f88672b7e88384f00',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '357d454c71fdb57c312ccc2f88672b7e88384f00',
+            'dev_requirement' => false,
+        ),
+    ),
+);
